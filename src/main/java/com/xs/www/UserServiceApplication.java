2d109cfb89e68dc81d16a4065e28b5cc7abac177
lenovo <lenovo@LAPTOP-JQUPS5LO>
2019-07-26 16:37:20 +0800

    修改userservice

--- src/main/java/com/xs/www/UserServiceApplication.java
+++ src/main/java/com/xs/www/UserServiceApplication.java
@@ -3,11 +3,11 @@ package com.xs.www;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableAutoConfiguration
 public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
